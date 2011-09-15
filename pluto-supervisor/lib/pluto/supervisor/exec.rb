76c30f43245b24f6983cabc39cfbc9ec505e5500
Simon Menke <simon.menke@gmail.com>
2011-09-15 15:46:20 +0200

    Better exec

--- pluto-supervisor/lib/pluto/supervisor/exec.rb
+++ pluto-supervisor/lib/pluto/supervisor/exec.rb
@@ -1,6 +1,31 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'yajl'
+require 'etc'
 
 args = Yajl::Parser.parse($stdin.gets)
-Process.exec(*args)
+env, command, pgroup, unsetenv_others, chdir, umask, close_others = *args
+
+if unsetenv_others
+  ENV.each do |key, _|
+    ENV.delete(key)
+  end
+end
+
+env.each do |key, value|
+  ENV[key] = value
+end
+
+if pgroup
+  Process.setpgrp
+end
+
+Dir.chdir(chdir)
+File.umask(umask)
+
+if Etc.getpwuid(Process.uid).name == 'root'
+  Process.euid = Etc.getpwnam('pluto').uid
+  Process.egid = Etc.getpwnam('pluto').gid
+end
+
+Process.exec(command)
